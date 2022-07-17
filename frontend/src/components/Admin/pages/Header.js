b0cc6633ba4966339d344b7cf17a41c1b20ad24e
SasinduDias <sasiwijegunasinghe98@gmail.com>
2022-07-17 13:58:08 +0530

    some file are added

--- frontend/src/components/Admin/pages/Header.js
+++ frontend/src/components/Admin/pages/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' 
+
 import '../css/Header.css';
 function Header(){
 
@@ -17,20 +20,20 @@ function Header(){
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">    
           <ul class="navbar-nav">
             <li class="nav-item">
-              <Link to="/" class="nav-link " aria-current="page" href="#">Home</Link>
+              <Link to="/" class="nav-link " aria-current="page" href="#"><i class="fa-solid fa-house"></i> Home</Link>
             </li>
             <li class="nav-item">
-              <Link to="/Notifications" class="nav-link" href="#">Notifications</Link>
+              <Link to="/Notifications" class="nav-link" href="#"><i class="fa-solid fa-bell"></i> Notifications</Link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">feedbacks & issues</a>
-            </li>
+            {/* <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa-solid fa-comment"></i>feedbacks & issues</a>
+            </li> */}
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Profile
+              <i class="fa-solid fa-user"></i>Profile
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <li><a class="dropdown-item" href="#">Go to Profile</a></li>
