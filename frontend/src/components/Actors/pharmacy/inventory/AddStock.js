046f9b61f44902fdf8736c08081488e7e415fd76
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2022-07-26 16:03:23 +0530

    Completed add inventory

--- frontend/src/components/Actors/pharmacy/inventory/AddStock.js
+++ frontend/src/components/Actors/pharmacy/inventory/AddStock.js
@@ -1,9 +1,32 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import { useEffect, useState } from "react";
 import './AddStock.css';
 
 function AddStock()
 {
+     // Adding medicine functions started here
+     const [serviceList, setServiceList] = useState([{ service: "" }]);
+
+     const handleServiceChange = (e, index) => {
+       const { name, value } = e.target;
+       const list = [...serviceList];
+       list[index][name] = value;
+       setServiceList(list);
+     };
+   
+     const handleServiceRemove = (index) => {
+       const list = [...serviceList];
+       list.splice(index, 1);
+       setServiceList(list);
+     };
+   
+     const handleServiceAdd = () => {
+       setServiceList([...serviceList, { service: "" }]);
+     };
+   //   Adding medicine functions ended here
+
+
     return(
         <div>
         <section className="py-4 ">
@@ -17,49 +40,181 @@ function AddStock()
         <div>
            
         </div>
-        <section className="section bg-c-light">
+        {/* Repeat Prescription Form started here */}
+        <section className="section bg-c-light border-bottom">
             <div className="container">
                 <div className="card shadow">
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-md-6 border-left">
-                                <h6>
-                                    Leave Us a Message
-                                </h6>
-                                <hr/>
-                                <div className="form-group">
-                                    <label className="mb-1">Name</label>
-                                    <input type="text" className="form-control" placeholder="Enter Your Name"></input>
-                                </div>
-                                <div className="form-group">
-                                    <label className="mb-1 mt-1">Email</label>
-                                    <input type="text" className="form-control" placeholder="Enter Your Email"></input>
-                                </div>
-                                <div className="form-group">
-                                    <label className="mb-1">Message</label>
-                                    <textarea rows="4" className="form-control" placeholder="Enter Your Message"></textarea>
-                                </div>
-                                <div className="form-group py-3">
-                                    <button type="button" className="btn btn-success shadow w-100 ">Send</button>
-                                </div>
-                            </div>
-                            <div className="col-md-6 border-start">
-                                <h5 className="main-heading">Address Information</h5>
-                                <div className="underline"></div>
-                                <p class="details">
-                                22 Palm Grove, Colombo 00300, Sri Lanka
-                                </p>
-                                <p class="details">+94 112 145 340</p>
-                                <p class="details">+94 112 145 341</p>
-                                <p class="details">info@petheaven.com</p>
-                                
-                            </div>
+                <div className="card-body">
+                <h5 className="main-heading text-center">New Stock Adding Form</h5>
+                <div className="underline mx-auto"></div>
+            
+                {/* <div className="form-group">
+                <div className="card">
+                    <div className="card-body bg-c-light">
+                    <div className="row ">
+                <div class="col-md-6">
+                    <label for="option" class="form-label">Medicine</label>
+                    <select class="form-control" id="inputGroupSelect02">
+                        <option selected>Select prescribed medicine</option>
+                        <option value="1">Cotrimaxazole 480 mg</option>
+                        <option value="2">Metronidazole 400 mg</option>
+                        <option value="3">Promethazine 10 mg</option>
+                        <option value="4">Ezo omeprazole 20 mg</option>
+                        <option value="5">Metronidazole 200 mg</option>
+                    </select>
+                    
+                    </div>
+                    <div class="col-md-3">
+                        <div className="form-group">
+                            <label className="mb-1">Quantity</label>
+                            <input type="text" className="form-control" placeholder="Enter quantity of medicine"></input>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div className="form-group">
+                            <label className="mb-1">Available Quantity</label>
+                            <input type="text" className="form-control" ></input>
                         </div>
                     </div>
                 </div>
-            </div>
+                    </div>
+                     
+                </div>
+                    
+                    
+                </div> */}
+                <div className="form-field">
+        <label htmlFor="service">Medicine List</label>
 
+        {/* Medicine input field started here */}
+        {serviceList.map((singleService, index) => (
+          <div key={index} className="services">
+            <div className="first-division">
+              {/* <input
+                name="service"
+                type="text"
+                id="service"
+                value={singleService.service}
+                onChange={(e) => handleServiceChange(e, index)}
+                required
+              /> */}
+
+                {/* Medicine input parts started here */}
+               <div className="form-group">
+                <div className="card">
+                    <div className="card-body bg-c-light">
+                    <div className="row ">
+                <div class="col-md-6">
+                    <label for="option" class="form-label">Medicine</label>
+                    <select class="form-control" id="inputGroupSelect02">
+                        <option selected>Select medicine to be added</option>
+                        <option value="1">Cotrimaxazole 480 mg</option>
+                        <option value="2">Metronidazole 400 mg</option>
+                        <option value="3">Promethazine 10 mg</option>
+                        <option value="4">Ezo omeprazole 20 mg</option>
+                        <option value="5">Metronidazole 200 mg</option>
+                    </select>
+                    
+                    </div>
+                    <div class="col-md-3">
+                        <div className="form-group">
+                            <label className="mb-1">Quantity</label>
+                            <input type="text" className="form-control" placeholder="Enter newly added quantity"></input>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div className="form-group">
+                            <label className="mb-1">Available Quantity</label>
+                            <input type="text" className="form-control" ></input>
+                        </div>
+                    </div>
+                </div>
+                    </div>
+                     
+                </div>
+                {/* Medicine input parts ended here */}
+                    
+                </div>
+
+              {serviceList.length - 1 === index && serviceList.length < 6 && (
+                <button
+                  type="button"
+                  onClick={handleServiceAdd}
+                  className="add-btn"
+                >
+                  <span>+Add a Medicine</span>
+                </button>
+              )}
+            </div>
+            <div className="second-division">
+              {serviceList.length !== 1 && (
+                <button
+                  type="button"
+                  onClick={() => handleServiceRemove(index)}
+                  className="remove-btn"
+                >
+                  <span>Remove</span>
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+        {/* Medicine input field ended here */}
+
+      </div>
+      {/* <div className="output">
+        <h2>Output</h2>
+        {serviceList &&
+          serviceList.map((singleService, index) => (
+            <ul key={index}>
+              {singleService.service && <li>{singleService.service}</li>}
+            </ul>
+          ))}
+      </div> */}
+      
+
+      {/* Confirm and reject buttons started here */}
+      <div className="form-group py-3 row ">
+            <div className="col-md-2">
+                <button type="button" id="submit-btn" className="btn shadow w-100 ">Submit</button>
+            </div>
+      </div>
+    {/* Confirm and reject buttons started here */}
+
+            
+                </div>
+                
+                </div>
+        
+                
+                {/* <form className="Prescription" autoComplete="off">
+                    <div className="form-field">
+                        <label htmlFor="service">
+                            service
+                        </label>
+                        <div className="services">
+                            <div className="first-devision">
+                                <input name="service" type="text" id="service" required/>
+                                <button type="button" className="add-btn">
+                                    <span>+Add a service</span>
+                                </button>
+                            </div>
+                            <div className="second-devision">
+                                <button type="button" className="remove-btn">
+                                    <span>Remove</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="output">
+                        <h2>Out put</h2>
+                    </div>
+                </form> */}
+                
+            </div>
+            
         </section>
+        {/* Repeat Prescription Form ended here */}
     </div>
     )
 }
