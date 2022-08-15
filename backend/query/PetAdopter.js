3de302f54da22ccd3bd88262ca2bac54ea4a00e6
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2022-08-15 10:09:10 +0530

    Completed signup and signin without otp

--- backend/query/PetAdopter.js
+++ backend/query/PetAdopter.js
@@ -1,3 +1,3 @@
-exports.CHECK_EMAIL = "SELECT * FROM students WHERE email =?";
-exports.REGISTER_PETADOPTER = "INSERT INTO students VALUES (NULL, ?)";
+// exports.CHECK_EMAIL = "SELECT * FROM students WHERE email =?";
+// exports.REGISTER_PETADOPTER = "INSERT INTO students VALUES (NULL, ?)";
 
