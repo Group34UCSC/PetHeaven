fea39f138fd3d631c3a5da233dec8a5dc955695a
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2022-08-18 11:32:53 +0530

    Updated post new advertisement

--- frontend/src/components/Actors/petstore/pages/PostAdvertisement.js
+++ frontend/src/components/Actors/petstore/pages/PostAdvertisement.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import '../css/PostAdvertisement.css';
 
 function PostAdvertisement()
@@ -6,10 +7,10 @@ function PostAdvertisement()
     return(
         <div>
             <section className="py-4 ">
-            <div className="container">
+            <div className="container col-md-8">
                 <div className="row bg-success text-white"> 
                     <div className="col-md-4 my-auto"></div>
-                    <h3 className="text-center">Post a New Advertisement</h3>
+                    <h3 className="text-center">New Advertisement</h3>
                 </div>
             </div>
             </section>
@@ -17,31 +18,49 @@ function PostAdvertisement()
                
             </div>
             <section className="section bg-c-light">
-                <div className="container">
+                <div className="container col-md-8">
                     <div className="card shadow">
                         <div className="card-body">
                             <div className="">
                             <div className="row">
-                                <div className="col-md-8 border-left">
-                                    <h6>
-                                        Leave Us a Message
-                                    </h6>
-                                    <hr/>
-                                    <div className="form-group">
-                                        <label className="mb-1">Name</label>
-                                        <input type="text" className="form-control" placeholder="Enter Your Name"></input>
+                                <div className="col-md-1">
+
+                                </div>
+                                <div className="col-md-10 ">
+                                    <h5 className="main-heading text-center">To Post a New Advertisement</h5>
+                                <div className="underline mx-auto"></div>
+                                    
+                                    <div className="form-group ">
+                                        <label className="mb-1">Title</label>
+                                        <input type="text" className="form-control" placeholder="Enter the title"></input>
                                     </div>
                                     <div className="form-group">
-                                        <label className="mb-1 mt-1">Email</label>
-                                        <input type="text" className="form-control" placeholder="Enter Your Email"></input>
+                                        <label className="mb-1 mt-2">Price</label>
+                                        <input type="text" className="form-control" placeholder="Enter the price(LKR)"></input>
                                     </div>
                                     <div className="form-group">
-                                        <label className="mb-1">Message</label>
-                                        <textarea rows="4" className="form-control" placeholder="Enter Your Message"></textarea>
+                                        <label className="mb-1 mt-2">Image</label>
+                                        <input type="file" multiple accept="image/*"  className="form-control" placeholder="Input a Image"></input>
                                     </div>
-                                    <div className="form-group py-3">
+                                    
+                                    <div className="form-group">
+                                        <label className="mb-1 mt-2">Description</label>
+                                        <textarea rows="4" className="form-control" placeholder="Enter a description"></textarea>
+                                    </div>
+                                    {/* <div className="form-group py-3">
                                         <button type="button" className="btn btn-success shadow w-100 ">Send</button>
+                                    </div> */}
+                                    <div className="d-flex justify-content-center mb-3 mt-4 ">
+                                        <Link to="/petstore/addNewEquipment" class="nav-link active">
+                                            {/* <button type="button" id="post-add-btn" className="btn shadow w-100 ">Post a New Advertisement</button> */}
+                                            <button type="button" id="submit-btn" className="btn shadow w-100 ">Post</button>
+                                        </Link>
                                     </div>
+                                    
+                                </div>
+
+                                <div className="col-md-1">
+                                    
                                 </div>
                                
                             </div>
