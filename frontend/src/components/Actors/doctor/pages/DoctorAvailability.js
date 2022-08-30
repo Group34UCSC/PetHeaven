391e1e7c6f99bceca637b4b1d33771729d9bb8d5
SasinduDias <sasiwijegunasinghe98@gmail.com>
2022-08-30 11:13:20 +0530

    doctor availability partially created

--- frontend/src/components/Actors/doctor/pages/DoctorAvailability.js
+++ frontend/src/components/Actors/doctor/pages/DoctorAvailability.js
@@ -6,147 +6,76 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' 
 import NavbarUsers from '../../../includes/NavbarUsers';
 import '../css/Doctoravailable.css';
-function DoctorAvailability(){
+
+
+
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2021, 6, 0),
+        end: new Date(2021, 6, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2021, 6, 7),
+        end: new Date(2021, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2021, 6, 20),
+        end: new Date(2021, 6, 23),
+    },
+];
+
+function DoctorAvailability() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
+    }
+
     return (
-        <div>  
-            <NavbarUsers/>
-            <div class="container py-5">
-            <header class="text-center text-white mb-5">
-                <h1 class="display-4">Bootstrap Calendar</h1>
-            </header>
-
-            <div class="calendar shadow bg-white p-5">
-            <div class="d-flex align-items-center"><i class="fa fa-calendar fa-3x mr-3"></i>
-                <h2 class="month font-weight-bold mb-0 text-uppercase">August 2022</h2>
+        <div className="App">
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
             </div>
-            <p class="font-italic text-muted mb-5">No events for this day.</p>
-            <ol class="day-names list-unstyled">
-                <li class="font-weight-bold text-uppercase">Sun</li>
-                <li class="font-weight-bold text-uppercase">Mon</li>
-                <li class="font-weight-bold text-uppercase">Tue</li>
-                <li class="font-weight-bold text-uppercase">Wed</li>
-                <li class="font-weight-bold text-uppercase">Thu</li>
-                <li class="font-weight-bold text-uppercase">Fri</li>
-                <li class="font-weight-bold text-uppercase">Sat</li>
-            </ol>
-
-  <ol class="days list-unstyled">
-    <li>
-      <div class="date">1</div>
-      <div class="event bg-success">Channel Mr.Silva</div>
-    </li>
-    <li>
-      <div class="date">2</div>
-    </li>
-    <li>
-      <div class="date">3</div>
-    </li>
-    <li>
-      <div class="date">4</div>
-    </li>
-    <li>
-      <div class="date">5</div>
-    </li>
-    <li>
-      <div class="date">6</div>
-    </li>
-    <li>
-      <div class="date">7</div>
-    </li>
-    <li>
-      <div class="date">8</div>
-    </li>
-    <li>
-      <div class="date">9</div>
-    </li>
-    <li>
-      <div class="date">10</div>
-    </li>
-    <li>
-      <div class="date">11</div>
-    </li>
-    <li>
-      <div class="date">12</div>
-    </li>
-    <li>
-      <div class="date">13</div>
-      <div class="event all-day begin span-2 bg-warning">Holiday</div>
-    </li>
-    <li>
-      <div class="date">14</div>
-    </li>
-    <li>
-      <div class="date">15</div>
-      <div class="event all-day end bg-success">Work in office</div>
-    </li>
-    <li>
-      <div class="date">16</div>
-    </li>
-    <li>
-      <div class="date">17</div>
-    </li>
-    <li>
-      <div class="date">18</div>
-    </li>
-    <li>
-      <div class="date">19</div>
-    </li>
-    <li>
-      <div class="date">20</div>
-    </li>
-    <li>
-      <div class="date">21</div>
-      <div class="event bg-primary">Channel with Ms Kaushi</div>
-      <div class="event bg-success">Pet  owner counceling</div>
-    </li>
-    <li>
-      <div class="date">22</div>
-      <div class="event bg-info">Patient meeting at hospital</div>
-    </li>
-    <li>
-      <div class="date">23</div>
-    </li>
-    <li>
-      <div class="date">24</div>
-    </li>
-    <li>
-      <div class="date">25</div>
-    </li>
-    <li>
-      <div class="date">26</div>
-    </li>
-    <li>
-      <div class="date">27</div>
-    </li>
-    <li>
-      <div class="date">28</div>
-    </li>
-    <li>
-      <div class="date">29</div>
-    </li>
-    <li>
-      <div class="date">30</div>
-    </li>
-    <li>
-      <div class="date">31</div>
-    </li>
-    <li class="outside">
-      <div class="date">1</div>
-    </li>
-    <li class="outside">
-      <div class="date">2</div>
-    </li>
-    <li class="outside">
-      <div class="date">3</div>
-    </li>
-    <li class="outside">
-      <div class="date">4</div>
-    </li>
-  </ol>
-</div>
-</div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 400, margin: "50px" }} />
         </div>
-    )
+    );
 }
 
-export default DoctorAvailability; 
+export default DoctorAvailability;
+
+
+
