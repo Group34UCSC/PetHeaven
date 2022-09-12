170e14328cb04cc53a49bbe281a40867b45d4d5c
SasinduDias <sasiwijegunasinghe98@gmail.com>
2022-09-13 00:00:06 +0530

    Doctor chat part rebuild

--- backend/routes/DoctorRoute.js
+++ backend/routes/DoctorRoute.js
@@ -6,6 +6,8 @@ const Doctor_controller = require('../controller/Doctor_controller');
 // DoctorRoute.post("/Delete/:user_id",Doctor_controller.Delete_Accounts);
 DoctorRoute.get("/View_Appointment",Doctor_controller.View_Appointment);
 DoctorRoute.post("/Consultation_accept",Doctor_controller.Consultation_accept);
+DoctorRoute.get("/Inbox_Table",Doctor_controller.Inbox_Table);
+DoctorRoute.post("/Done/:appointment_ID",Doctor_controller.Done);
 
 
 module.exports = DoctorRoute;
