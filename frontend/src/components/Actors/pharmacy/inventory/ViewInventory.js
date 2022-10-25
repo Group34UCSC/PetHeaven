d9ff8988f2996a6dc5ab5e73f8e30813c1cdda6c
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2022-10-25 08:04:09 +0530

    Completed frontend for update inventory

--- frontend/src/components/Actors/pharmacy/inventory/ViewInventory.js
+++ frontend/src/components/Actors/pharmacy/inventory/ViewInventory.js
@@ -1,9 +1,19 @@
 import React, {useEffect,useState}  from "react";
 import NavbarUsers from "../../../includes/NavbarUsers";
 import './ViewInventory.css';
+import SignUpImg from "../../../images/pharmacy1.jpg";
+
 
 function ViewInventory()
 {
+    const [med, setMed] = useState([])
+    const [MFD, setMFD] = useState("");
+    const [EXP, setEXP] = useState("");
+    const [Quantity, setQuantity] = useState("");
+    const [Price, setPrice] = useState("");
+
+    const [buttontext, setButtontext] = useState('Update');
+    
     const [inventory,setInventory]=useState([])
     useEffect(()=>{
         fetch("http://localhost:5000/pharmacy/viewInventory").then((result)=>{
@@ -14,6 +24,41 @@ function ViewInventory()
         })
     })},[])
     const[searchTerm, setSearchTerm]=useState('')
+
+    console.warn(med)
+    function UpdateStock(item) {
+        console.log(item)
+        setMFD(item.MFD)
+        setEXP(item.EXP)
+        setQuantity(item.Quantity)
+        setPrice(item.Price)
+
+    }
+
+    const UpdateStockform = async e => {
+        e.preventDefault();
+        setButtontext('Updated..');
+        try {
+            const body = { MFD,EXP , Quantity, Price };
+            const response = await fetch(
+                "http://localhost:5000/Admin/Update",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify(body)
+                }
+            );
+            console.log(response);
+        }
+        catch (err) {
+            console.log("Falil12");
+        }
+
+    }
+
+    
     return(
         <div>
             <NavbarUsers/>
@@ -25,6 +70,129 @@ function ViewInventory()
                 </div>
             </div>
             </section>
+
+            <section className="section bg-c-light" >
+                    <div className="container">
+                        <div className="card shadow">
+                            <div className="card-body">
+                                <div className="row">
+
+
+
+
+                                    <div className="col-md-6 border-left">
+
+                                       
+                                        <div className="col-md-12">
+                                            <div className="card shadow">
+                                                <div className="card-body">
+                                                    <div className="row bg-success text-white mb-3">
+                                                        <div className="col-md-4 my-auto"></div>
+                                                        <h3 className="text-center">Update Stock Details</h3>
+                                                    </div>
+
+                                                </div>
+                                                <img src={SignUpImg}  alt="Services"/>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+
+                                    <div className="col-md-6 border-start">
+                                 
+                                        <section id="SignUpSection">
+                                            
+                                            <form id="SignUpForm" >
+                     
+                                         
+        
+                                              
+                                            <label htmlFor="username" class="labels">
+                                                    Manufacture Date:
+                                                   
+                                                </label>
+                                                <input class="inputFields"
+                                                    type="date"
+                                                    id="MFD"
+                                                    value={MFD} onChange={(e)=>{setMFD(e.target.value)}}
+                                                    placeholder="Enter Manufacture Date"
+                                                    required
+                                               
+                                                />
+
+
+
+                                                 
+                                              <label htmlFor="EXP" class="labels">
+                                                Expiary Date:
+                                                   
+                                                </label>
+                                                <input class="inputFields"
+                                                    type="date"
+                                                    id="username"
+                                                    value={EXP} onChange={(e)=>{setEXP(e.target.value)}}
+                                                    placeholder="Enter Expiary Date"
+                                                    required
+                                               
+                                                />
+
+
+                                              <label htmlFor="Quantity" class="labels">
+                                                Quantity:
+                                                   
+                                                </label>
+                                                <input class="inputFields"
+                                                    type="text"
+                                                    id="usertype"
+                                                    value={Quantity} onChange={(e)=>{setQuantity(e.target.value)}}
+                                                    placeholder="Enter Quantity"
+                                                    required
+                                               
+                                                />
+                                               
+
+                                                <label htmlFor="Price" class="labels">
+                                                    Price:
+                                                    
+                                                </label>
+                                                <input class="inputFields"
+                                                    type="text"
+                                                    id="Price"
+                                                    value={Price} onChange={(e)=>{setPrice(e.target.value)}}
+                                                    placeholder="Enter Unit Price"
+                                                    required
+                                                    
+                                                />
+                                               
+
+                                               
+                                                <div className="d-flex justify-content-center">
+
+                                                    <button className="mt-5" id="SignUpBtn" onClick={UpdateStockform}>{buttontext}</button>
+                                                </div>
+
+
+                                            </form>
+                                        </section>
+                                        {/* //    )}  */}
+
+                                    </div>
+
+
+
+
+
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </section>
             
         {/* Medicine stock details started here    */}
         <section className="section bg-c-light border-bottom">
@@ -89,6 +257,7 @@ function ViewInventory()
 
                 
                 </div>
+                
 
                 {/* Stock details started here */}
                 <div className="container mb-3">
@@ -100,13 +269,16 @@ function ViewInventory()
                             <tr>
                             <th scope="col"></th>
                             <th scope="col">Medicine Name</th>
+                            <th scope="col">Manufacture Date</th>
+                            <th scope="col">Expiary Date</th>
                             <th scope="col">Quantity(Units)</th>
                             <th scope="col">Unit Price(LKR)</th>
+                            <th scope="col">Operation</th>
                             </tr>
                         </thead>
                         <tbody>
-
                             {
+                                
                                 inventory.filter((item)=>{
                                     if(searchTerm == ""){
                                         return item
@@ -115,10 +287,14 @@ function ViewInventory()
                                     }
                                 }).map((item,i)=>
                                     <tr key={i}>
+                                        
                                 <td>{i+1}</td>
                                 <td>{item.Medicine}</td>
+                                <td>{item.MFD}</td>
+                                <td>{item.EXP}</td>
                                 <td>{item.Quantity}</td>
                                 <td>{item.Price}</td>
+                                <td><button type="button" class="btn btn-warning" onClick={() => UpdateStock(item)}>Update</button></td>
                                 {/* <td>{item.Medicine}</td> */}
                                 </tr>
                                 )
